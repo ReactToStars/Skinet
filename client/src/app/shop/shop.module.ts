c846c7c3341d283c65bf392211926acc45369df6
Hunghandsome <tronghung26102000@gmail.com>
2023-06-10 16:17:59 +0700

    End of section 10

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,19 +3,20 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ShopItemComponent } from './shop-item/shop-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ShopRoutingModule } from './shop-routing.module';
 
 
 @NgModule({
   declarations: [
     ShopComponent,
-    ShopItemComponent
+    ShopItemComponent,
+    ProductDetailsComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
-  ],
-  exports: [
-    ShopComponent
+    SharedModule,
+    ShopRoutingModule
   ]
 })
 export class ShopModule { }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -15,6 +15,10 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
+  getProduct(id: number){
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+
   getProducts(shopParams: ShopParams):Observable<Pagination<Product[]>>{
     let params = new HttpParams();
    
