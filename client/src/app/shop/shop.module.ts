538cc7e3bdf3b448f9ada34347d31a98ffc10483
Hunghandsome <tronghung26102000@gmail.com>
2023-06-17 17:35:27 +0700

    End of section 14

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
-import { ShopItemComponent } from './shop-item/shop-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ShopRoutingModule } from './shop-routing.module';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 
 @NgModule({
   declarations: [
     ShopComponent,
-    ShopItemComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    ProductItemComponent
   ],
   imports: [
     CommonModule,
