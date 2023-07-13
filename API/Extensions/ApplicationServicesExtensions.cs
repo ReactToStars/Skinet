c40d6e29072376015ba21a494141a5b7dc5286e8
bita <tronghung26102000@gmail.com>
2023-07-13 21:54:15 +0700

    add settings.json

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -50,7 +50,7 @@ namespace API.Extensions
             services.AddCors(opt => 
             {
                 opt.AddPolicy("CorsPolicy", policy => {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200", "http://localhost:4200");
                 });
             });
 
